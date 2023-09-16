4fe796a1c827a8e75e876f59188d7d438325dcfd
Oualid elf <oualid.elferaoui@gmail.com>
2023-09-16 23:47:21 +0100

    feat: Connect to Apollo Server.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allBooks {
+      author
+      title
+      genres
+    }
+  }
+`
+
+client.query({ query })
+  .then((res) => {
+    console.log(res.data)
+  })
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
