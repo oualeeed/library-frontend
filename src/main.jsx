8b1829ecfec58b4f6efab2ed89ff836246b09120
Oualid elf <oualid.elferaoui@gmail.com>
2023-09-17 18:35:52 +0100

    style: configured prettier.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
-})
+});
 
 const query = gql`
   query {
@@ -15,11 +15,10 @@ const query = gql`
       genres
     }
   }
-`
+`;
 
-client.query({ query })
-  .then((res) => {
-    console.log(res.data)
-  })
+client.query({ query }).then((res) => {
+  console.log(res.data);
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
