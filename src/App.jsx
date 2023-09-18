4ae1895eb3b86d0ad462d50c00beba4700699180
Oualid elf <oualid.elferaoui@gmail.com>
2023-09-18 12:55:37 +0100

    feat: styled the nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import { Link, Route, Routes } from 'react-router-dom';
 import AuthorsView from './components/AuthorsView';
 import BooksView from './components/BooksView';
 import './App.css';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
-      <div>
-        <Link to="/books">Books</Link>
-        <Link to="/authors">Authors</Link>
-      </div>
+      <nav className="nav-bar">
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/books">
+          Books
+        </Link>
+        <Link className="link" to="/authors">
+          Authors
+        </Link>
+      </nav>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/authors" element={<AuthorsView />} />
         <Route path="/books" element={<BooksView />} />
       </Routes>
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <h2>
+      Welcome! We&apos;ve got some nerdy books for you. Have Fun, Mr. book worm
+      🤓
+    </h2>
+  );
+};
+
+export default Home;
